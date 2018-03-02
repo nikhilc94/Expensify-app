d268d7276268f0f0cab2d43b9a457d3077927f57
Nikhil Chelani <nikhilchelani.94@gmail.com>
2018-03-02 18:38:14 +0400

    Updated summary component to show count of expenses hidden due to filters

--- src/components/ConfirmationModal.js
+++ src/components/ConfirmationModal.js
@@ -11,7 +11,7 @@ const ConfirmationModal = ({ isOpen, closeModal, onRemove, expenseDescription })
 	>
 			<h3 className="modal__message">Are you sure you want to delete the expense?</h3>
 			<h3 className="modal__item">{`"${expenseDescription}"`}</h3>
-			<button className="button button--delete button-1" onClick={onRemove}>Delete</button>
+			<button className="button button--delete button-1" onClick={onRemove}>Delete Expense</button>
 			<button className="button button-2" onClick={closeModal}>Cancel</button>
 	</Modal>
 );
--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -5,7 +5,8 @@ import numeral from 'numeral';
 import visibleExpenses from '../selectors/expenses.js';
 import totalExpenses from '../selectors/expenses-total.js';
 
-const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+const ExpensesSummary = ({ expenseCount, expensesTotal, totalExpenseCount }) => {
+	const filteredExpenseCount = totalExpenseCount - expenseCount;
 	const expenseWord = expenseCount > 1 ? 'expenses' : 'expense';
 	const amount = numeral(expensesTotal / 100).format('$0,0.00');
 	return (
@@ -14,6 +15,10 @@ const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
 				<h1 className="page-header__title">
 					Viewing <span>{expenseCount}</span> {expenseWord} totalling <span>{amount}</span>
 				</h1>
+				{ 	filteredExpenseCount === 0 ? 
+					<h3 className="page-header__title">Showing all expenses.</h3> : 
+					<h3 className="page-header__title">Not showing <span>{filteredExpenseCount}</span> expenses because of filters.</h3> 
+				}
 				<div className="page-header__actions">
 					<Link className="button" to="/create">Add Expense</Link>
 				</div>
@@ -25,7 +30,8 @@ const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
 const mapStateToProps = (state) => {
 	return {
 		expenseCount: visibleExpenses(state.expenses, state.filters).length,
-		expensesTotal: totalExpenses(visibleExpenses(state.expenses, state.filters))
+		expensesTotal: totalExpenses(visibleExpenses(state.expenses, state.filters)),
+		totalExpenseCount: state.expenses.length
 	}
 }
 
